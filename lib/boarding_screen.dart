c32fbe50793255dc81ed7f3a3943e41fb222a9cc
Ehsanur-Rahman246 <rahmanehsanur246@gmail.com>
2026-03-08 19:56:29 +0600

    dev updated

--- lib/boarding_screen.dart
+++ lib/boarding_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-import 'package:step_progress/step_progress.dart';
+import 'package:timeline_tile/timeline_tile.dart';
 import 'package:core_care/main.dart';
 
 class OnboardingScreen extends StatefulWidget {
@@ -77,14 +77,14 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
-  late StepProgressController _stepController;
+  late PageController _pageController;
+  bool onLastScreen = false;
   final int _totalSteps = 10;
-  int currStep = 0;
 
   @override
   void initState() {
     super.initState();
-    _stepController = StepProgressController(totalSteps: _totalSteps);
+    _pageController = PageController();
   }
 
   @override
@@ -92,49 +92,118 @@ class _SignupPageState extends State<SignupPage> {
     return Scaffold(
       body: Stack(
         children: [
-          Center(
-            child: IconButton(onPressed:() {}, icon: Icon(Icons.eighteen_mp)),
-          ),
-
-          Positioned(
-            left: 0,
-            right: 0,
-            top: 20,
-            child: StepProgress(totalSteps: _totalSteps,
-              nodeIconBuilder: (index, currStep){
-              if(index < currStep){
-                return Icon(Icons.check_circle_outline, size: 12, color: Theme.of(context).colorScheme.onPrimary,);
-              }
-              else if(index == currStep) {
-                return Icon(Icons.circle, size: 12, color: CustomColors.greenOutline(context),);
-              }
-              else{
-                return Icon(Icons.circle_outlined, size: 15, color: CustomColors.greyLight(context),);
-              }
-              },
-              theme: StepProgressThemeData(
-
-                defaultForegroundColor: CustomColors.greyDark(context),
-                activeForegroundColor: Theme.of(context).colorScheme.primary,
-                stepLineStyle: StepLineStyle(
-                  lineThickness: 2,
-                ),
-                stepNodeStyle: StepNodeStyle(
-
-                ),
+          PageView(
+            physics: const NeverScrollableScrollPhysics(),
+            onPageChanged: (index){
+                onLastScreen = (index == _totalSteps - 1);
+                },
+            controller: _pageController,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    color: Colors.red,
+                  ),
+                  TimelineTile(
+                    isFirst: true,
+                  ),
+                ],
               ),
-              width: MediaQuery.of(context).size.width * 0.8,
-            stepNodeSize: 15,
-            controller: _stepController,
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 15),
-              onStepChanged: (index){
-              setState(() {
-                currStep = index;
-              });
-              },
-            ),
+              Stack(
+                children: [
+                  Container(
+                    color: Colors.blue,
+                  ),
+                  TimelineTile(),
+                ],
+              ),
+              Stack(
+                children: [
+                  Container(
+                    color: Colors.green,
+                  ),
+                  TimelineTile(),
+                ],
+              ),
+              Stack(
+                children: [
+                  Container(
+                    color: Colors.yellow,
+                  ),
+                  TimelineTile(),
+                ],
+              ),
+              Stack(
+                children: [
+                  Container(
+                    color: Colors.white,
+                  ),
+                  TimelineTile(),
+                ],
+              ),
+              Stack(
+                children: [
+                  Container(
+                    color: Colors.black,
+                  ),
+                  TimelineTile(),
+                ],
+              ),
+              Stack(
+                children: [
+                  Container(
+                    color: Colors.purple,
+                  ),
+                  TimelineTile(),
+                ],
+              ),
+              Stack(
+                children: [
+                  Container(
+                    color: Colors.orange,
+                  ),
+                  TimelineTile(),
+                ],
+              ),
+              Stack(
+                children: [
+                  Container(
+                    color: Colors.grey,
+                  ),
+                  TimelineTile(),
+                ],
+              ),
+              Stack(
+                children: [
+                  Container(
+                    color: Colors.cyan,
+                  ),
+                  TimelineTile(
+                    isLast: true,
+                  ),
+                ],
+              ),
+            ],
           ),
+          IconButton.filled(onPressed: (){}, icon: Icon(Icons.navigate_next)),
         ],
+      ),
+    );
+  }
+  Widget stepIndicator(bool isFirst, bool isLast, bool isActive){
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.85,
+      child: TimelineTile(
+        isFirst: isFirst,
+        isLast: isLast,
+        axis: TimelineAxis.horizontal,
+        beforeLineStyle: LineStyle(
+          color: Theme.of(context).colorScheme.primary
+        ),
+        indicatorStyle: IndicatorStyle(
+          color: Theme.of(context).colorScheme.primary,
+          iconStyle: IconStyle(iconData: Icons.check, color: Theme.of(context).colorScheme.onPrimary),
+        ),
       ),
     );
   }
