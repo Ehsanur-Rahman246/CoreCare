3a1c6bb87723bd91ae8a6daf708aac2b408ce288
Ehsanur-Rahman246 <rahmanehsanur246@gmail.com>
2026-03-17 22:54:01 +0600

    adding chips & tags

---
+++ lib/tags.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Tags extends StatelessWidget {
+  const Tags({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class Chips extends StatelessWidget {
+  const Chips({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
