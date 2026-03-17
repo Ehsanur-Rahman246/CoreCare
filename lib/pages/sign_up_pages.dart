413febbbd5b0c2813df49a7986e9e2bf81fcee0d
Ehsanur-Rahman246 <rahmanehsanur246@gmail.com>
2026-03-17 19:55:05 +0600

    adding signup pages

--- lib/pages/sign_up_pages.dart
+++ lib/pages/sign_up_pages.dart
@@ -1,5 +1,136 @@
 import 'package:flutter/material.dart';
 
-class SignUpPages {
-  
+class SignupPageOne extends StatefulWidget {
+  const SignupPageOne({super.key});
+
+  @override
+  State<SignupPageOne> createState() => _SignupPageOneState();
+}
+
+class _SignupPageOneState extends State<SignupPageOne> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class SignupPageTwo extends StatefulWidget {
+  const SignupPageTwo({super.key});
+
+  @override
+  State<SignupPageTwo> createState() => _SignupPageTwoState();
+}
+
+class _SignupPageTwoState extends State<SignupPageTwo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class SignupPageThree extends StatefulWidget {
+  const SignupPageThree({super.key});
+
+  @override
+  State<SignupPageThree> createState() => _SignupPageThreeState();
+}
+
+class _SignupPageThreeState extends State<SignupPageThree> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class SignupPageFour extends StatefulWidget {
+  const SignupPageFour({super.key});
+
+  @override
+  State<SignupPageFour> createState() => _SignupPageFourState();
+}
+
+class _SignupPageFourState extends State<SignupPageFour> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class SignupPageFive extends StatefulWidget {
+  const SignupPageFive({super.key});
+
+  @override
+  State<SignupPageFive> createState() => _SignupPageFiveState();
+}
+
+class _SignupPageFiveState extends State<SignupPageFive> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class SignupPageSix extends StatefulWidget {
+  const SignupPageSix({super.key});
+
+  @override
+  State<SignupPageSix> createState() => _SignupPageSixState();
+}
+
+class _SignupPageSixState extends State<SignupPageSix> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class SignupPageSeven extends StatefulWidget {
+  const SignupPageSeven({super.key});
+
+  @override
+  State<SignupPageSeven> createState() => _SignupPageSevenState();
+}
+
+class _SignupPageSevenState extends State<SignupPageSeven> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class SignupPageEight extends StatefulWidget {
+  const SignupPageEight({super.key});
+
+  @override
+  State<SignupPageEight> createState() => _SignupPageEightState();
+}
+
+class _SignupPageEightState extends State<SignupPageEight> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class SignupPageNine extends StatefulWidget {
+  const SignupPageNine({super.key});
+
+  @override
+  State<SignupPageNine> createState() => _SignupPageNineState();
+}
+
+class _SignupPageNineState extends State<SignupPageNine> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class SignupPageTen extends StatelessWidget {
+  const SignupPageTen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
